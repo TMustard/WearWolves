52ae554552cfa7c4ff7240a9a18500e79b70e6d7
mfoster1989 <malcolmfoster@gmail.com>
2017-12-20 12:35:08 -0700

    added images

--- data.js
+++ data.js
@@ -4,7 +4,7 @@ mainOutfits = [{
     outfitDescription: "It’s 6pm and you’re just leaving the office, but you have tickets to Les Mis with your alpha female? No need to worry! We’ve got your formalwear decisions under control.  In 2017, more than a few alpha male weres chose blue as their swerve of choice. The color isn't a crazy choice in the tailoring world, having emerged as a go-to for wolves looking to stand out on red carpets in the last few years, but that doesn't mean there aren't degrees to which you can measure the style move's success (too bright and it looks chintzy, not bright enough and it's hardly a swerve at all)"
 }, {
     occasion: "Wedding",
-    image: "http://rachelaclingen.com/wp-content/uploads/2015/04/wedding-ceremonies-toronto.png",
+    image: "http://impactdigital.com/wp-content/uploads/2014/01/JR_Mens_Journal_Nov_Style_Shot3_050_07_11.jpg",
     outfitDescription: "Wedding: Wedding season is coming and with it, piles of invitations that should clue you in on what to wear to your werepals' upcoming nuptials.  But if you're the type of Wolf used to putting on his 'wedding suit' no matter who's getting married, receiving a suggestion to show up in your best 'Beach Formal' gear can be confusing. And, let's be honest, annoying. A gray or navy suit is going to be your best bet here. Pair it with a proper dress shirt (no button-down collars, please) and solid or subtle patterned tie to keep things on the dressier side."
 }, {
     occasion: "Golf",
