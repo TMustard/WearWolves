7921f96583baffcc63aa2b546e01ae39b53b36b1
mfoster1989 <malcolmfoster@gmail.com>
2017-12-20 12:31:37 -0700

    added images

--- data.js
+++ data.js
@@ -1,26 +1,26 @@
 mainOutfits = [{
     occasion: "Opera",
-    image:"",
+    image:"https://img00.deviantart.net/a2bc/i/2011/038/e/1/tux_by_novawuff-d392dxm.jpg",
     outfitDescription: "It’s 6pm and you’re just leaving the office, but you have tickets to Les Mis with your alpha female? No need to worry! We’ve got your formalwear decisions under control.  In 2017, more than a few alpha male weres chose blue as their swerve of choice. The color isn't a crazy choice in the tailoring world, having emerged as a go-to for wolves looking to stand out on red carpets in the last few years, but that doesn't mean there aren't degrees to which you can measure the style move's success (too bright and it looks chintzy, not bright enough and it's hardly a swerve at all)"
 }, {
     occasion: "Wedding",
-    image: "",
+    image: "http://rachelaclingen.com/wp-content/uploads/2015/04/wedding-ceremonies-toronto.png",
     outfitDescription: "Wedding: Wedding season is coming and with it, piles of invitations that should clue you in on what to wear to your werepals' upcoming nuptials.  But if you're the type of Wolf used to putting on his 'wedding suit' no matter who's getting married, receiving a suggestion to show up in your best 'Beach Formal' gear can be confusing. And, let's be honest, annoying. A gray or navy suit is going to be your best bet here. Pair it with a proper dress shirt (no button-down collars, please) and solid or subtle patterned tie to keep things on the dressier side."
 }, {
     occasion: "Golf",
-    image:"",
+    image:"http://thegolfmentalcoach.com/wp-content/uploads/2014/06/iStock_000019017330Small.jpg",
     outfitDescription: "In recent years, many WereWolf clothing labels have became focused on functionality, and forgot that golf, at its core, is a sport that has always valued style. Walter Hagen? Dandy. Ben Hogan? Stoic. Arnold Palmer? Sex symbol. All those wolves wore classic sportswear on the course, and as a Wolf on the go, you should do the same.  If you're going to tee it up, you needn't look like you're running a marathon or going to space, you simply need to stick to a a few basics.  A pastel polo and a pair of slim chinos will be perfect whether you’re playing or spectating.",
 }, {
     occasion: "Phish Concert",
-    image:"",
+    image:"https://www.amazingamazon.com.au/media/catalog/product/cache/1/image/900x900/9df78eab33525d08d6e5fb8d27136e95/r/a/rarefish.jpg",
     outfitDescription: "WereWolf 'phans' have more money than the broke, unwashed, travelin’ light hippie wolves they’re often made out to be.  They also care more about style, and one joy of phandom is getting the inside joke when you see fellow Weres decked out in painstakingly designed bootleg tees.  Those are actually just the beginning.  As with the band’s setlists, style at a Phish show is all over the place. But it’s rarely ever boring.  Fit will always be your first priority, but this is the time to get loud with your color choices and combinations.  That outer-space themed hoodie that you never thought went with your fur color? Its time to bust it out.  Worried that your pants don’t match? Ask the Wolf in front of you with the dilated pupils if he cares..."
 }, {
     occasion: "First Date",
-    image: "",
-    outfitDescription: ""
+    image: "http://www.pd4pic.com/images/calendar-date-month-year-red.png",
+    outfitDescription: "For any Wolf, the first five dates are a literal minefield of inopportune sweating, nervous rambling, hesitant touching—god help us all—and regular-old excitement. Add to that some last-minute panic of what you should wear, and you might just give up and order takeout and pretend you have a headache.  BUT, if you’re the type of Wolf who lives for the hunt, here are a few basics you should consider to look particularly lupine.  Female Weres love a Wolf in a henley, slim flat-front chinos and boots.  Always consider boots as she might be in heels, they’ll give you an extra one to two inches of height and make you stand up straight.  Avoid hats, unless you are trying to hide from an ex-Wolf, in which case you might want to worry less about your clothes and more about what you’ve done."
 }, {
     occasion: "Hipster",
-    image:"",
+    image:"http://1.bp.blogspot.com/-cbX7BWp_wbU/US2Nn65lGiI/AAAAAAAAADs/_Ify6WTfqI8/s1600/hipster2.jpg",
     outfitDescription: "Hipster Werewolves are often defined by what they wear (and what they don’t), and what they wear is often far removed from what the rest of us in the civilized world would choose to put on. But, in the midst of terrible cut-off tees, too-short shorts, and potentially offensive head dresses, there are a few bright spots.  Wolves these days going for the hipster look are modeling themselves after WerePharrell, sporting caps, stonewash jeans, vests and high top sneakers over their paws.",
 }],
 
